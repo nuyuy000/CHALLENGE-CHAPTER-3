5582587733de4de48c87d4e67a3717ef2573c9d6
nuyuy000 <nurul.180170137@mhs.unimal.ac.id>
2022-03-23 06:28:54 +0700

    Button Go To Screen 2 jalan

--- app/src/main/java/com/example/chapter33/fragmentTiga.kt
+++ app/src/main/java/com/example/chapter33/fragmentTiga.kt
@@ -5,19 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.coroutines.currentCoroutineContext
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [fragmentTiga.newInstance] factory method to
- * create an instance of this fragment.
- */
 class fragmentTiga : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -37,17 +32,13 @@ class fragmentTiga : Fragment() {
         return inflater.inflate(R.layout.fragment_tiga, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragmentTiga.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
+
         fun newInstance(param1: String, param2: String) =
             fragmentTiga().apply {
                 arguments = Bundle().apply {
